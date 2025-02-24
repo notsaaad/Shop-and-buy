5d7947d8e00bb0568fbcc11a1d975b7417ebdd9f
notsaaad <https://github.com/notsaaad/sawyancom.git>
2025-02-25 01:50:45 +0200

    some updates

--- front end/js/main.js
+++ front end/js/main.js
@@ -1,4 +1,28 @@
 
+$(document).ready(function(){ // Every Password input add icon to show password when clicked
+  $('.password-with-icon input[type="password"]').after(`<span class="eye password_eye" > <i class="fa-solid fa-eye icon"></i></span>`);
+});
+
+
+    //Show Passowrd when Click on  Eye icon
+    $(document).ready(function(){
+      let passwords = $('.password_eye');
+      passwords.each( function(i , ele) {
+        $(ele).on('click', () =>{
+          $(this).attr('type')
+          let type =  $(this).siblings('input').attr('type');
+          type == 'password' ? $(this).siblings('input').attr('type', 'text')             : $(this).siblings('input').attr('type', 'password');
+          type == 'password' ? $(this).html(`<i class="fa-solid fa-eye-slash icon"></i>`) : $(this).html(`<i class="fa-solid fa-eye icon"></i>`);
+        })
+      } )
+    });
+
+
+
+
+
+
+
 const menuButton = document.getElementById('menuButton');
 const sidebar = document.getElementById('sidebar');
 
